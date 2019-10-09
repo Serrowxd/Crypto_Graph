1201bd4a40346db685278c7e0875010630acf64d
Kevin Jolley <serrowpls@gmail.com>
2019-10-09 02:04:11 -0500

    landing page wip

--- front/src/components/Landing.js
+++ front/src/components/Landing.js
@@ -4,7 +4,14 @@ import { Charts } from '../shared/shareReduce';
 function Landing() {
   return (
     <div className="landing-wrapper">
-      <Charts />
+      <div className="jumbo">
+        <h1>Crypto Graph</h1>
+        <p>Your #1 Choice for Real-Time Crypto Analysis</p>
+        <div className="jumbo-buttons">
+          <div className="btn">Existing User</div>
+          <div className="btn">Create Account</div>
+        </div>
+      </div>
     </div>
   );
 }
