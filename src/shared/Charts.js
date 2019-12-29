aa4dae17f4d273b6c02b98c0438f02c0aab00297
Kevin Jolley <serrowpls@gmail.com>
2019-12-29 02:55:07 -0600

    chart updated

--- src/shared/Charts.js
+++ src/shared/Charts.js
@@ -1,134 +1,110 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Chart from 'react-apexcharts';
 
-class Charts extends Component {
-  constructor(props) {
-    super(props);
+function Charts(props) {
+  // Theme Object
+  const colorTheme = {
+    light: {
+      white: '#f5f5f5',
+      whiteish: '#e0e0e0',
+      whiter: '#9e9e9e',
+    },
+    dark: {
+      black: '#2f2f2f',
+      blacker: '#242424',
+      blackish: '#424242',
+    },
+    colors: {
+      blue: '#00bcd4',
+      orange: '#e6a800',
+      green: '#8bc34a',
+    },
+  };
 
-    // Theme Object
-    const colorTheme = {
-      light: {
-        white: '#f5f5f5',
-        whiteish: '#e0e0e0',
-        whiter: '#9e9e9e',
-      },
-      dark: {
-        black: '#2f2f2f',
-        blacker: '#242424',
-        blackish: '#424242',
-      },
-      colors: {
-        blue: '#00bcd4',
-        orange: '#e6a800',
-        green: '#8bc34a',
-      },
-    };
-
-    // ChartJS Please Work
-    // X-Axis lines will not respond to color updates
-    // Documentation is trash
-    this.state = {
-      optionsMixedChart: {
-        grid: {
-          show: true,
-          borderColor: props.themeSwap ? colorTheme.dark.black : colorTheme.light.white,
-          strokeDashArray: 0,
-          position: 'back',
-          xaxis: {
-            lines: {
-              show: false,
-            },
-          },
-          yaxis: {
-            lines: {
-              show: true,
-            },
-          },
-          row: {
-            colors: undefined,
-            opacity: 0.5,
-          },
-          column: {
-            colors: undefined,
-            opacity: 0.5,
-          },
-          padding: {
-            top: 0,
-            right: 10,
-            bottom: 0,
-            left: 10,
-          },
-        },
-        chart: {
-          id: 'bar',
-          foreColor: props.themeSwap ? colorTheme.dark.black : colorTheme.light.white,
-          toolbar: {
+  const options = {
+    optionsMixedChart: {
+      grid: {
+        show: true,
+        borderColor: props.themeSwap ? colorTheme.dark.black : colorTheme.light.white,
+        strokeDashArray: 0,
+        position: 'back',
+        xaxis: {
+          lines: {
             show: false,
           },
         },
-        plotOptions: {
-          bar: {
-            columnWidth: '50%',
-            endingShape: 'arrow',
-          },
-        },
-        stroke: {
-          width: [4, 0, 0],
-        },
-        xaxis: {
-          categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        },
-        markers: {
-          size: 6,
-          strokeWidth: 3,
-          fillOpacity: 0,
-          strokeOpacity: 0,
-          hover: {
-            size: 8,
-          },
-        },
         yaxis: {
-          tickAmount: 5,
-          min: 0,
-          max: 100,
+          lines: {
+            show: true,
+          },
+        },
+        row: {
+          colors: undefined,
+          opacity: 0.5,
+        },
+        column: {
+          colors: undefined,
+          opacity: 0.5,
+        },
+        padding: {
+          top: 0,
+          right: 10,
+          bottom: 0,
+          left: 10,
         },
       },
-      seriesMixedChart: [
-        {
-          name: 'series-1',
-          type: 'line',
-          data: [30, 40, 25, 50, 49, 21, 70, 51],
+      chart: {
+        id: 'bar',
+        foreColor: props.themeSwap ? colorTheme.dark.black : colorTheme.light.white,
+        toolbar: {
+          show: false,
         },
-        {
-          name: 'series-2',
-          type: 'column',
-          data: [23, 12, 54, 61, 32, 56, 81, 19],
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '50%',
+          endingShape: 'arrow',
         },
-        {
-          name: 'series-3',
-          type: 'column',
-          data: [62, 12, 45, 55, 76, 41, 23, 43],
+      },
+      stroke: {
+        width: [4, 0, 0],
+      },
+      xaxis: {
+        categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      markers: {
+        size: 6,
+        strokeWidth: 3,
+        fillOpacity: 0,
+        strokeOpacity: 0,
+        hover: {
+          size: 8,
         },
-      ],
-    };
-  }
+      },
+      yaxis: {
+        tickAmount: 5,
+        min: 0,
+        max: 100,
+      },
+    },
+    seriesMixedChart: [
+      {
+        name: '',
+        type: 'line',
+        data: [30, 40, 25, 50, 49, 21, 70, 51],
+      },
+    ],
+  };
 
-  render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="col mixed-chart">
-            <Chart
-              options={this.state.optionsMixedChart}
-              series={this.state.seriesMixedChart}
-              type="line"
-              width="700"
-            />
-          </div>
+  return (
+    <div className="app">
+      <div className="row">
+        <div className="col mixed-chart">
+          <Chart options={options.optionsMixedChart} series={options.seriesMixedChart} type="line" width="700" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Charts;
--- src/shared/shareReduce.js
+++ src/shared/shareReduce.js
@@ -2,6 +2,5 @@ import Navigation from './Nav';
 import Charts from './Charts';
 import LoginModal from './LoginModal';
 import FeedEntry from './FeedEntry';
-import PChart from './newChart';
 
-export { Navigation, Charts, LoginModal, FeedEntry, PChart };
+export { Navigation, Charts, LoginModal, FeedEntry };
